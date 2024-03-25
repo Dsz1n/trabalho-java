0077696a4b3c0635993d437c2e2dc4838a489825
Aaron Santos <santosaaron046@gmail.com>
2024-03-25 00:58:56 -0300

    atvjava

---
+++ atv4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Atv4 {
+
+    public void ExecutarPrograma() {
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número: ");
+        int num1 = ler.nextInt();
+
+        System.out.println("Informe o segundo número: ");
+        int num2 = ler.nextInt();
+
+        int soma = num1 + num2;
+        int subtracao = num1 - num2;
+        int multiplicacao = num1 * num2;
+        int divisao = num1 / num2;
+
+        System.out.println("A soma desses dois números é: " + soma);
+        System.out.println("A subtracao desses dois números é: " + subtracao);
+        System.out.println("A multiplicação desses dois números é: " + multiplicacao);
+        System.out.println("A divisão desses dois números é: " + divisao);
+
+        ler.close();
+    }
+
+}
